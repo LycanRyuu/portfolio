413d5348710905673550feadd98dd7b1c1c57cfa
Amol Mathur <amolmathur4869@gmail.com>
2023-06-28 00:28:34 +0530

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import "./App.css";
+import About from "./components/About";
+import MainContent from "./components/MainContent";
+
+function App() {
+	return (
+		<div className='App'>
+			<About />
+			<MainContent />
+		</div>
+	);
+}
+
+export default App;
---
+++ src/components/MainContent.jsx
@@ -0,0 +1,8 @@
+// import React from "react";
+import styles from "../styles/MainContent.module.css";
+
+const MainContent = () => {
+	return <div className={styles.MainContent}>MainContent</div>;
+};
+
+export default MainContent;
